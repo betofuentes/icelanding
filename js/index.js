c04336a8c50c604d92889d8acd6ef09e1820bd2c
Miguel Valdez <mike.281299@gmail.com>
2021-03-31 15:09:23 -0700

    Añadido form de pruebas + función que envía los parámetros en él

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-function sendEmail() {
+ function sendEmail() {
+	var name = document.getElementById("name").value;
+	var client_email = document.getElementById("client_email").value;
+	var body = document.getElementById("body").value;
+	var reason = document.getElementById("id_razon").value;
+
 	Email.send({
 	Host: "smtp.gmail.com",
 	Username : "6f.valdez.rodarte.miguel@gmail.com",
 	Password : "Hola123456!",
-	To : 'alberto.fuentesco@gmail.com',
+	To : 'mike.281299@gmail.com',
 	From : "6f.valdez.rodarte.miguel@gmail.com",
 	Subject : "Form.header",
-	Body : "Form.body",
+	Body : "Nombre del interesado: " + name + "\n Contacto: " + client_email + "Razón de contacto: " + reason + "Comentario del interesado: " + body,
 	}).then(
 		message => alert("mail sent successfully")
 	);
