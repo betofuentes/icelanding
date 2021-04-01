a4ae988fd38243ffb122e66ed2c8f4d9f806556b
Miguel Valdez <mike.281299@gmail.com>
2021-03-31 20:33:05 -0700

    Añadida validación de correo electrónico

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
+ function check_email(email){
+	var pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"; 
+	 if(email.value.match(pattern))
+	 {
+		 sendEmail();
+	 }else{
+		alert("Parece que este correo no es válido.")
+		document.getElementById("client_email").focus();
+		return true;	
+	 }
+ }
+  
  function sendEmail() {
 	var name = document.getElementById("name").value;
 	var client_email = document.getElementById("client_email").value;
 	var body = document.getElementById("body").value;
 	var reason = document.getElementById("id_razon").value;
+	var client_number = document.getElementById("client_number").value;
 
 	Email.send({
 	Host: "smtp.gmail.com",
@@ -10,9 +23,9 @@
 	Password : "Hola123456!",
 	To : 'mike.281299@gmail.com',
 	From : "6f.valdez.rodarte.miguel@gmail.com",
-	Subject : "Form.header",
-	Body : "Nombre del interesado: " + name + "\n Contacto: " + client_email + "Razón de contacto: " + reason + "Comentario del interesado: " + body,
+	Subject : "[" + reason + "] " + name + " desea ponerse en contacto con usted.",
+	Body : "Nombre del interesado: " + name + "<br>Correo electrónico: " + client_email + "<br>Número telefónico: +52 " + client_number + "<br>Razón de contacto: " + reason + "<br>Comentario del interesado: " + body,
 	}).then(
-		message => alert("mail sent successfully")
+		message => alert("¡El correo ha sido mandado exitosamente! En breve nos pondremos en contacto con usted.")
 	);
 }
