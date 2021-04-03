0e9b2db244bfbd737781f6028eb27a483a303393
Sezaru-Yamamoto <yamamotoherrera.cesaralejandro@utacapulco.edu.mx>
2021-04-03 01:10:51 -0600

    Se agregó el slider

--- js/index.js
+++ js/index.js
@@ -14,14 +14,52 @@ close.addEventListener('click', function(){
     modal.style.display = 'none';
 });
 
-// To close clicking out of modal
-window.addEventListener('click', function(e){
-    console.log(e.target);
-    if(e.target == flex){
-        modal.style.display = 'none';
-    }
+// Slider actions
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next(){
+	let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+	slider.style.marginLeft = "-200%";
+	slider.style.transition = "all 0.5s";
+	setTimeout(function(){
+		slider.style.transition = "none";
+		slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+		slider.style.marginLeft = "-100%";
+	}, 500);
+}
+
+function Previous(){
+	let sliderSection = document.querySelectorAll(".slider__section");
+	let sliderSectionLast = sliderSection[sliderSection.length -1];
+	slider.style.marginLeft = "0";
+	slider.style.transition = "all 0.5s";
+	setTimeout(function(){
+		slider.style.transition = "none";
+		slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+		slider.style.marginLeft = "-100%";
+	}, 500);
+}
+
+btnRight.addEventListener('click', function(){
+	Next();
 });
 
+btnLeft.addEventListener('click', function(){
+	Previous();
+});
+
+//Function to slide automatically, 6000 = 6s
+setInterval(() => {
+	Next();
+}, 6000);
  function check_email(email){
 	var pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"; 
 	 if(email.value.match(pattern))
