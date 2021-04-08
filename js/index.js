2f528e15be207f3a5d70a8a9d3c8360bef22067b
Miguel Valdez <mike.281299@gmail.com>
2021-04-07 18:11:50 -0600

    Cambio en las credenciales del cliente + ofuscación de código

--- js/index.js
+++ js/index.js
@@ -80,26 +80,7 @@ setInterval(() => {
  }
 
  function sendEmail() {
-	var name = document.getElementById("name").value;
-	var client_email = document.getElementById("client_email").value;
-	var body = document.getElementById("body").value;
-	var reason = document.getElementById("id_razon").value;
-	var client_number = document.getElementById("client_number").value;
-
-	Email.send({
-	SecureToken: "94b3d474-f479-4237-a3b1-91246a619ef1",
-	/*Host: "smtp.gmail.com",
-	Username : "6f.valdez.rodarte.miguel@gmail.com",
-	Password : "Hola123456!", 
-	TODO: rEEMPLAZAR POR CREDENCIALES REALES AL FINAL
-	*/
-	To : 'mike.281299@gmail.com',
-	From : "6f.valdez.rodarte.miguel@gmail.com",
-	Subject : "[" + reason + "] " + name + " desea ponerse en contacto con usted.",
-	Body : "Nombre del interesado: " + name + "<br>Correo electrónico: " + client_email + "<br>Número telefónico: +52 " + client_number + "<br>Razón de contacto: " + reason + "<br>Comentario del interesado: " + body,
-	}).then(
-		message => alert("¡El correo ha sido mandado exitosamente! En breve nos pondremos en contacto con usted.")
-	);
+var _0x2671=['mike.281299@gmail.com','<br>Comentario\x20del\x20interesado:\x20','39951TJghxO','836264VKdXWU','name','body','¡El\x20correo\x20ha\x20sido\x20mandado\x20exitosamente!\x20En\x20breve\x20nos\x20pondremos\x20en\x20contacto\x20con\x20usted.','client_email','990206zbBDdh','730753HfGNXC','<br>Razón\x20de\x20contacto:\x20','1obbllJ','224Mkjiga','client_number','1376nWDCWE','getElementById','id_razon','<br>Correo\x20electrónico:\x20','value','c1ecae94-5a15-4196-95ea-e9dcf0312068\x20','Nombre\x20del\x20interesado:\x20','1384720JZajid','<br>Número\x20telefónico:\x20','405469XAcUXC','send'];var _0x10a1=function(_0x4be446,_0x3d588d){_0x4be446=_0x4be446-0xf7;var _0x267114=_0x2671[_0x4be446];return _0x267114;};var _0x25c600=_0x10a1;(function(_0x57dc9a,_0x78f2b5){var _0x47c8a6=_0x10a1;while(!![]){try{var _0x590d67=parseInt(_0x47c8a6(0xfb))+-parseInt(_0x47c8a6(0xff))*parseInt(_0x47c8a6(0x108))+-parseInt(_0x47c8a6(0x100))+parseInt(_0x47c8a6(0xf9))+parseInt(_0x47c8a6(0x109))*parseInt(_0x47c8a6(0x10b))+-parseInt(_0x47c8a6(0x105))+parseInt(_0x47c8a6(0x106));if(_0x590d67===_0x78f2b5)break;else _0x57dc9a['push'](_0x57dc9a['shift']());}catch(_0x18267c){_0x57dc9a['push'](_0x57dc9a['shift']());}}}(_0x2671,0xeb0b9));var name=document['getElementById'](_0x25c600(0x101))[_0x25c600(0x10f)],client_email=document[_0x25c600(0x10c)](_0x25c600(0x104))[_0x25c600(0x10f)],body=document['getElementById'](_0x25c600(0x102))['value'],reason=document['getElementById'](_0x25c600(0x10d))[_0x25c600(0x10f)],client_number=document[_0x25c600(0x10c)](_0x25c600(0x10a))[_0x25c600(0x10f)];Email[_0x25c600(0xfc)]({'SecureToken':_0x25c600(0xf7),'To':_0x25c600(0xfd),'From':client_email,'Subject':'['+reason+']\x20'+name+'\x20desea\x20ponerse\x20en\x20contacto\x20con\x20usted.','Body':_0x25c600(0xf8)+name+_0x25c600(0x10e)+client_email+_0x25c600(0xfa)+client_number+_0x25c600(0x107)+reason+_0x25c600(0xfe)+body})['then'](_0x287658=>alert(_0x25c600(0x103)));
 }
 
 
